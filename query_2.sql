b1960a728ec839fd011cb7e187fa684e045328a3
Tassigulias <gulias@me.com>
2026-09-22 21:52:02 -0300

    Feat: adição de 2 colunas
    Adição das colunas de job_title e employee_id.

--- query_2.sql
+++ query_2.sql
@@ -1,5 +1,7 @@
-SELECT hr.employees.salary, hr.countries.country_name, hr.regions.region_name, hr.departments.department_name
+SELECT hr.jobs.job_title, hr.employees.employee_id, hr.employees.salary, hr.countries.country_name, hr.regions.region_name
 FROM hr.EMPLOYEES
+LEFT JOIN hr.jobs
+ON hr.jobs.job_id = hr.employees.job_id
 LEFT JOIN hr.departments
 ON hr.departments.department_id = hr.employees.department_id
 LEFT JOIN hr.locations
